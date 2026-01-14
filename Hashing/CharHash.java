c850beb530a2718e545be3233711965db0876cbd
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-14 23:51:22 +0530

    Hashing-int array freq

---
+++ Hashing/CharHash.java
@@ -0,0 +1,10 @@
+import java.util.*;
+// package Hashing;
+
+public class CharHash {
+    public static void main(String args[]){
+        String s="abcdabef";
+        char[] query={'a','b','d'};
+        int[] hash=new int[query.length+1];
+    }
+}
---
+++ Hashing/Freqofno.java
@@ -0,0 +1,23 @@
+import java.util.*;// package Hashing;
+
+public class Freqofno {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int[] a={1,2,1,3,3};
+        int[] hash=new int[13];
+        //pre-storing
+        for(int i=0;i<4;i++){
+            hash[a[i]]+=1;
+        }
+
+        //fetching
+        int q=sc.nextInt();//no of elements to be queried
+        while(q-- !=0){
+            int num=sc.nextInt();//input the query
+            System.out.println(hash[num]);
+        }
+    
+                
+    }
+}//here we need to check the max element in the given array and query  then declare the hash array size
+//normally the hash array size is chosen by doing something , ill descover later
