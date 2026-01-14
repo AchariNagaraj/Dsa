0ef76b9e6062823dbc748ae2f44871fbfb274cfd
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-15 00:12:29 +0530

    Hashing: Char hashing , string input

--- Hashing/CharHash.java
+++ Hashing/CharHash.java
@@ -3,8 +3,24 @@ import java.util.*;
 
 public class CharHash {
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
         String s="abcdabef";
-        char[] query={'a','b','d'};
-        int[] hash=new int[query.length+1];
+        
+        int[] hash=new int[26];
+        //storing
+        for(int i=0;i<s.length();i++){
+            int asci_coded=(int)s.charAt(i)-97;
+            hash[asci_coded]+=1;
+        }
+
+        //query
+        int q=sc.nextInt();
+        while(q-- !=0){
+            int n=(int)sc.next().charAt(0)-97;//take the character, type convert to ascival, subtract 97 for indexing
+            
+            System.out.println(hash[n]);
+        }
+
+
     }
 }
