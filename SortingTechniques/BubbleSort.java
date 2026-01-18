9c70f8d2d84855d10c6c71f22acb2344a9bb04a2
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-18 13:52:21 +0530

    optimized bubble sort( the best case is improved to O(n)

--- SortingTechniques/BubbleSort.java
+++ SortingTechniques/BubbleSort.java
@@ -4,13 +4,18 @@ import java.util.*;
 public class BubbleSort {
     public static void main(String args[]){
         int nums[]={4,2,7,5,6};
+        boolean swap=false;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i;j+1<nums.length-i;j++){
                 if(nums[j]>nums[j+1]){
                     int c=nums[j];
                     nums[j]=nums[j+1];
                     nums[j+1]=c;
+                    swap=true;
                 }
+            }
+            if(i==0 && swap==false){
+                break;
             }
         }
         System.out.println(Arrays.toString(nums));
