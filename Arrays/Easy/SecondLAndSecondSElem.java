f10ceee07e2fd4f95cb387db0ebb6e623f13215a
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-22 18:53:50 +0530

    updation in logic of second largest and second smalles

--- Arrays/Easy/SecondLAndSecondSElem.java
+++ Arrays/Easy/SecondLAndSecondSElem.java
@@ -3,19 +3,22 @@ public class SecondLAndSecondSElem {
         int [] arr={10,1,2};
         int max=arr[0],min=arr[0],smax=arr[1],smin=arr[1];
         for(int i=1;i<arr.length;i++){
-            if(arr[i]>smax)
-                smax=arr[i];
+            //max
             if(arr[i]>max){
                 smax=max;
                 max=arr[i];
             }
-            if(arr[i]<smin)
-                smin=arr[i];
+            else if(arr[i]>smax)
+                smax=arr[i];
+
+
+            //min
             if(arr[i]<min){
                 smin=min;
                 min=arr[i];
             }
-            
+            else if(arr[i]<smin)
+                smin=arr[i];
         }
         if(max==min)
             System.out.println(-1);
