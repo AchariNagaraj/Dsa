75119309e31d648ba25cddcdaf17042ba1fc9a53
AchariNagaraj <acharinagaraj19@gmail.com>
2026-02-17 16:41:13 +0530

    optimized the trapping rain water problem to O(n)

--- Arrays/Pattern_wise/TwoPointers/TrappingRainWater.java
+++ Arrays/Pattern_wise/TwoPointers/TrappingRainWater.java
@@ -1,19 +1,26 @@
 import java.util.*;
 public class TrappingRainWater {
     static int trap(int[] height) {
+           //lmax points to height of max left and rmax points to height of max right
         int n=height.length;
-        int lmax=0,twater=0;
+        int lmax=height[0],twater=0;
+        int rmax=height[n-1];
+        int[] arr=new int[n];     
+        int x=1,max=n-1;
+        arr[0]=max;
+        //storing max rights in an array
+        for(int j=n-2;j>=1;j--){
+            if(height[j]>=height[max]){
+                max=j;
+                arr[x++]=max;
+            }
+        }
+        x--;//point to right max index
         for(int k=1;k<n-1;k++){
-            int rmax=n-1;
-            for(int i=lmax;i<=k;i++){
-                if(height[i]>=height[lmax]) 
-                    lmax=i;
-            }
-            for(int j=n-1;j>=k;j--){
-                if(height[j]>=height[rmax])
-                    rmax=j;
-            }
-            int water=Math.min(height[lmax],height[rmax])-height[k];
+            if(k>arr[x]) x--;        //if k crosses the current max right index then change the max right index
+            lmax=Math.max(lmax,height[k]);
+            rmax=height[arr[x]];
+            int water=Math.min(lmax,rmax)-height[k];
             twater+=water;
         }
         return twater;
@@ -33,5 +40,4 @@ public class TrappingRainWater {
 }
 //core idea: water at index k is :
 //water=min(max left height,max right height) - height[k]
-
-//but the time complexity of the code is O(n2) , so we need to optimize
+//optimized to O(n) but used extra space for storing the right maxes
